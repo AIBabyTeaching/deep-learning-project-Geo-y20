809ecd68e5a04f3ab6937985ddd79072a4874c2f
George Youhana <73796671+Geo-y20@users.noreply.github.com>
2023-12-28 23:41:02 +0200

    last update for html and css files and updated app.py
    enhance app.py 
    and still need for handling routing between pages 
    and adding more details for index.html to be more professional way

--- app.py
+++ app.py
@@ -1,34 +1,25 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify, render_template, send_from_directory
 from werkzeug.utils import secure_filename
 import os
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.resnet_v2 import preprocess_input
 import numpy as np
+import cv2
+import tensorflow as tf
 
 app = Flask(__name__)
 
-# Define the directory to store uploaded files
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-# Function to check if the file extension is allowed
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg'}
-
-# Load your trained model
+# Function to load your model
 def load_your_model():
-    model_path = 'StandardOCR-ResNet50V2-2.h5'  # Replace with your model path
-    model = load_model(model_path)
+    model = tf.keras.models.load_model('StandardOCR-ResNet50V2-2.h5')
     return model
 
 # Function to preprocess the uploaded image
 def preprocess_image(image_path):
-    img = image.load_img(image_path, target_size=(224, 224))
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array = preprocess_input(img_array)  # Preprocess for ResNet50V2
-    return img_array
+    img = cv2.imread(image_path)
+    img = cv2.resize(img, (120, 120))
+    img = img.astype('float32')
+    img /= 255.0
+    img = np.expand_dims(img, axis=0)
+    return img
 
 # Function to perform prediction
 def perform_prediction(filepath):
@@ -38,18 +29,32 @@ def perform_prediction(filepath):
     predicted_class_index = np.argmax(predictions, axis=1)[0]  # Get the predicted class index
     return predicted_class_index  # Return the predicted class index
 
+# Define the directory to store uploaded files
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Function to check if the file extension is allowed
+def allowed_file(filename):
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/predict', methods=['POST'])
-def predict():
+def upload_file():
+    # Check if a file was posted
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'})
 
     file = request.files['file']
 
-    if file.filename == '':
+    if not file:
         return jsonify({'error': 'No selected file'})
 
     if file and allowed_file(file.filename):
@@ -57,14 +62,29 @@ def predict():
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(filepath)
 
-        predicted_class_index = perform_prediction(filepath)  # Get the predicted class index
-        # Convert class index to class name based on your class list
-        class_names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-        predicted_class_name = class_names[predicted_class_index]
+        try:
+            # Perform prediction on the uploaded image
+            predicted_class_index = perform_prediction(filepath)
+            
+            # Define the class names corresponding to the indices
+            class_names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+            
+            # Convert the predicted class index to class name
+            predicted_class_name = class_names[predicted_class_index]
 
-        return jsonify({'prediction': predicted_class_name})  # Return the predicted class name
+            # Return the predicted class name
+            return jsonify({'prediction': predicted_class_name})
+
+        except Exception as e:
+            # If there's an error during prediction, return the error message
+            return jsonify({'error': str(e)})
 
     return jsonify({'error': 'File not allowed or unsupported format'})
+
+# Route to serve the script.js file
+@app.route('/js/<path:filename>')
+def serve_js(filename):
+    return send_from_directory('static/js', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -4,20 +4,38 @@ function predictCharacter() {
   const predictionResult = document.getElementById('predictionResult');
 
   const file = fileInput.files[0];
+  const formData = new FormData();
+  formData.append('file', file);
+
+  const url = '/predict'; // Assuming there's only one prediction endpoint
+
+  fetch(url, {
+    method: 'POST',
+    body: formData
+  })
+    .then(response => response.json())
+    .then(data => {
+      updatePredictionUI(predictionResult, data);
+      displayImagePreview(imagePreview, file);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+function updatePredictionUI(element, prediction) {
+  element.textContent = `Prediction: ${prediction.prediction}`;
+}
+
+function displayImagePreview(element, file) {
   const reader = new FileReader();
 
-  reader.onload = function(event) {
+  reader.onload = function (event) {
     const img = new Image();
     img.src = event.target.result;
 
-    // Display uploaded image
-    imagePreview.innerHTML = '';
-    imagePreview.appendChild(img);
-
-    // Send image for predictions (your existing logic here)
-    // For now, using a placeholder prediction
-    const prediction = { prediction: 'A' };
-    updatePredictionUI(predictionResult, prediction);
+    element.innerHTML = '';
+    element.appendChild(img);
   };
 
   if (file) {
@@ -25,6 +43,17 @@ function predictCharacter() {
   }
 }
 
-function updatePredictionUI(element, prediction) {
-  element.textContent = `Prediction: ${prediction.prediction}`;
-}
+// Event listener for scrolling to sections
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
